93524f5d9098c7c905f29a0a8a04e1106b9423f6
Eduardo San Martin Morote <posva13@gmail.com>
2018-02-07 17:09:50 +0100

    Use lowercase for schema

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,9 +1,9 @@
 const mongoose     = require('mongoose');
 const Schema       = mongoose.Schema;
 
-const RestaurantSchema = new Schema({
+const restaurantSchema = new Schema({
   name: String,
   description: String,
 });
 
-module.exports = mongoose.model('Restaurant', RestaurantSchema);
+module.exports = mongoose.model('Restaurant', restaurantSchema);
