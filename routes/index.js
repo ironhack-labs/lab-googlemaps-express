2f073de642ce0528e0954d7898f11f13d41d93cc
sandrabosk <sandraboskovic@hotmail.com>
2018-11-06 21:08:09 -0500

    update routes

--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,11 @@ router.get('/new', (req, res, next) => {
 // POST => to create new restaurant and save it to the DB
 router.post('/', (req, res, next) => {
   // add location object here
-  let location = {
-    type: 'Point',
-    coordinates: [req.body.longitude, req.body.latitude]
-  };
+  
 
 	const newRestaurant = new Restaurant({
-		name:        req.body.name,
-    description: req.body.description,
-    location: location //added!!!
+		name: req.body.name,
+		description: req.body.description
 	});
 
 	newRestaurant.save((error) => {
