865e4a85c24a382881956635250d11a3a8b3cb73
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-07-06 16:38:08 -0500

    lab solved

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -42,6 +42,43 @@ let markers =[]
   }
   getRestaurants();
 
+
+
+//////////
+/* 
+const geocoder = new google.maps.Geocoder();
+ 
+document.getElementById('submit').addEventListener('click', () => {
+  geocodeAddress(geocoder, map);
+});
+ 
+function geocodeAddress(geocoder, resultsMap) {
+  const address = document.getElementById('address').value;
+ 
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      let marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+      document.getElementById('latitude').value = results[0].geometry.location.lat();
+      document.getElementById('longitude').value = results[0].geometry.location.lng();
+    } else {
+      console.log(`Geocode was not successful for the following reason: ${status}`);
+    }
+  });
+}
+ */
+
+
+
+
+
+
+
+
+
 });
 
 
